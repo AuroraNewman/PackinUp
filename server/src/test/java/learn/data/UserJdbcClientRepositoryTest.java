32009a009806d20ff463a52b645f653c3e008466
Aurora Newman <auroranewmansoftware@gmail.com>
2025-03-17 20:48:39 -0400

    tested unhappy paths

--- server/src/test/java/learn/data/UserJdbcClientRepositoryTest.java
+++ server/src/test/java/learn/data/UserJdbcClientRepositoryTest.java
@@ -29,12 +29,26 @@ class UserJdbcClientRepositoryTest {
     }
 
     @Test
+    void failToFindBadId(){
+        User actual = repository.findById(TestHelper.badId);
+
+        assertNull(actual);
+    }
+
+    @Test
     void findByEmail() {
         User actual = repository.findByEmail(TestHelper.existingUser.getEmail());
 
         assertNotNull(actual);
         assertEquals(1, actual.getUserId());
         assertEquals(actual, TestHelper.existingUser);
+    }
+
+    @Test
+    void failToFindByEmailBadEmail(){
+        User actual = repository.findByEmail(TestHelper.tooLongEmail);
+
+        assertNull(actual);
     }
 
     @Test
@@ -54,10 +68,30 @@ class UserJdbcClientRepositoryTest {
     }
 
     @Test
+    void failToUpdateUsernameBadId() {
+        User failUser = new User();
+        failUser.setUserId(TestHelper.badId);
+        failUser.setUsername("Bernard");
+        failUser.setPassword(TestHelper.goodPassword);
+        failUser.setEmail(TestHelper.goodEmail);
+
+        boolean actual = repository.updateUsername(failUser, "Bernard");
+
+        assertFalse(actual);
+    }
+
+    @Test
     void delete() {
         boolean actual = repository.delete(TestHelper.existingUser.getUserId());
 
         assertTrue(actual);
         assertNull(repository.findById(TestHelper.existingUser.getUserId()));
+    }
+
+    @Test
+    void failToDeleteBadId(){
+        boolean actual = repository.delete(TestHelper.badId);
+
+        assertFalse(actual);
     }
 }
