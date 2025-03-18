dd78e1da4dd4024d5fe90d88ff0d84804d616d78
Aurora Newman <auroranewmansoftware@gmail.com>
2025-03-18 13:30:46 -0400

    tested create in controller level

--- server/src/test/java/learn/controllers/UserControllerTest.java
+++ server/src/test/java/learn/controllers/UserControllerTest.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import learn.TestHelper;
 import learn.models.User;
 import learn.service.Result;
+import learn.service.ResultType;
 import learn.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
@@ -21,6 +22,8 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -72,6 +75,215 @@ class UserControllerTest {
                     .contentType(MediaType.APPLICATION_JSON)
                     .content(userJson))
                     .andExpect(MockMvcResultMatchers.status().isCreated());                    ;
+        }
+        @Test
+        void shouldReturn400WhenEmpty() throws Exception {
+            var request = post(PREFIX)
+                    .contentType(MediaType.APPLICATION_JSON);
+
+            mvc.perform(request)
+                    .andExpect(status().isBadRequest());
+        }
+        @Test
+        void addShouldReturn400WhenInvalid() throws Exception {
+        User user = new User();
+        String userJson = jsonMapper.writeValueAsString(user);
+        var request = post(PREFIX)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(userJson);
+        mvc.perform(request)
+                .andExpect(status().isBadRequest());
+        }
+        @Test
+        void addShouldReturn415WhenMultipart() throws Exception {
+            String userJson = jsonMapper.writeValueAsString(testUser);
+
+            var request = post(PREFIX)
+                    .contentType(MediaType.MULTIPART_FORM_DATA)
+                    .content(userJson);
+            mvc.perform(request)
+                    .andExpect(status().isUnsupportedMediaType());
+        }
+        //invalid if no lowers, no uppsers, no digits, no specials
+        @Test
+        void shouldReturn400OnBlankUsername() throws Exception {
+            User failAdd = new User();
+            failAdd.setUserId(0);
+            failAdd.setUsername("");
+            failAdd.setEmail(TestHelper.goodEmail);
+            failAdd.setPassword(TestHelper.goodPassword);
+
+            Result<User> expectedResult = new Result<>();
+            expectedResult.addErrorMessage("Username is required.", ResultType.INVALID);
+
+            when(service.create(failAdd)).thenReturn(expectedResult);
+
+            mvc.perform(MockMvcRequestBuilders.post(PREFIX)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(jsonMapper.writeValueAsString(failAdd)))
+                    .andExpect(status().isBadRequest());
+        }
+        @Test
+        void shouldReturn400OnLongUsername() throws Exception {
+            User failAdd = new User();
+            failAdd.setUserId(0);
+            failAdd.setUsername(TestHelper.tooLongUsername);
+            failAdd.setEmail(TestHelper.goodEmail);
+            failAdd.setPassword(TestHelper.goodPassword);
+
+            Result<User> expectedResult = new Result<>();
+            expectedResult.addErrorMessage("Username must be fewer than 50 characters.", ResultType.INVALID);
+
+            when(service.create(failAdd)).thenReturn(expectedResult);
+
+            mvc.perform(MockMvcRequestBuilders.post(PREFIX)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(jsonMapper.writeValueAsString(failAdd)))
+                    .andExpect(status().isBadRequest());
+        }
+        @Test
+        void shouldReturn400OnLongEmail() throws Exception {
+            User failAdd = new User();
+            failAdd.setUserId(0);
+            failAdd.setUsername(TestHelper.goodUsername);
+            failAdd.setEmail(TestHelper.tooLongEmail);
+            failAdd.setPassword(TestHelper.goodPassword);
+
+            Result<User> expectedResult = new Result<>();
+            expectedResult.addErrorMessage("Email must be fewer than 100 characters.", ResultType.INVALID);
+
+            when(service.create(failAdd)).thenReturn(expectedResult);
+
+            mvc.perform(MockMvcRequestBuilders.post(PREFIX)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(jsonMapper.writeValueAsString(failAdd)))
+                    .andExpect(status().isBadRequest());
+        }
+        @Test
+        void shouldReturn400OnBlankEmail() throws Exception {
+            User failAdd = new User();
+            failAdd.setUserId(0);
+            failAdd.setUsername(TestHelper.goodUsername);
+            failAdd.setEmail("");
+            failAdd.setPassword(TestHelper.goodPassword);
+
+            Result<User> expectedResult = new Result<>();
+            expectedResult.addErrorMessage("Email is required.", ResultType.INVALID);
+
+            when(service.create(failAdd)).thenReturn(expectedResult);
+
+            mvc.perform(MockMvcRequestBuilders.post(PREFIX)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(jsonMapper.writeValueAsString(failAdd)))
+                    .andExpect(status().isBadRequest());
+        }
+        @Test
+        void shouldReturn400OnBlankPassword() throws Exception {
+            User failAdd = new User();
+            failAdd.setUserId(0);
+            failAdd.setUsername(TestHelper.goodUsername);
+            failAdd.setEmail(TestHelper.goodEmail);
+            failAdd.setPassword("");
+
+            Result<User> expectedResult = new Result<>();
+            expectedResult.addErrorMessage("Password is required.", ResultType.INVALID);
+
+            when(service.create(failAdd)).thenReturn(expectedResult);
+
+            mvc.perform(MockMvcRequestBuilders.post(PREFIX)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(jsonMapper.writeValueAsString(failAdd)))
+                    .andExpect(status().isBadRequest());
+        }
+        @Test
+        void shouldReturn400OnInvalidPasswordNoCaps() throws Exception {
+            User failAdd = new User();
+            failAdd.setUserId(0);
+            failAdd.setUsername(TestHelper.goodUsername);
+            failAdd.setEmail(TestHelper.goodEmail);
+            failAdd.setPassword(TestHelper.badFormatPasswordNoCaps);
+
+            Result<User> expectedResult = new Result<>();
+            expectedResult.addErrorMessage("Password must contain at least one uppercase letter, one lowercase letter, one special character, and one number.", ResultType.INVALID);
+
+            when(service.create(failAdd)).thenReturn(expectedResult);
+
+            mvc.perform(MockMvcRequestBuilders.post(PREFIX)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(jsonMapper.writeValueAsString(failAdd)))
+                    .andExpect(status().isBadRequest());
+        }
+        @Test
+        void shouldReturn400OnInvalidPasswordNoLowers() throws Exception {
+            User failAdd = new User();
+            failAdd.setUserId(0);
+            failAdd.setUsername(TestHelper.goodUsername);
+            failAdd.setEmail(TestHelper.goodEmail);
+            failAdd.setPassword(TestHelper.badFormatPasswordNoLowers);
+
+            Result<User> expectedResult = new Result<>();
+            expectedResult.addErrorMessage("Password must contain at least one uppercase letter, one lowercase letter, one special character, and one number.", ResultType.INVALID);
+
+            when(service.create(failAdd)).thenReturn(expectedResult);
+
+            mvc.perform(MockMvcRequestBuilders.post(PREFIX)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(jsonMapper.writeValueAsString(failAdd)))
+                    .andExpect(status().isBadRequest());
+        }
+        @Test
+        void shouldReturn400OnInvalidPasswordNoNumber() throws Exception {
+            User failAdd = new User();
+            failAdd.setUserId(0);
+            failAdd.setUsername(TestHelper.goodUsername);
+            failAdd.setEmail(TestHelper.goodEmail);
+            failAdd.setPassword(TestHelper.badFormatPasswordNoNumber);
+
+            Result<User> expectedResult = new Result<>();
+            expectedResult.addErrorMessage("Password must contain at least one uppercase letter, one lowercase letter, one special character, and one number.", ResultType.INVALID);
+
+            when(service.create(failAdd)).thenReturn(expectedResult);
+
+            mvc.perform(MockMvcRequestBuilders.post(PREFIX)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(jsonMapper.writeValueAsString(failAdd)))
+                    .andExpect(status().isBadRequest());
+        }
+        @Test
+        void shouldReturn400OnInvalidPasswordNoSpecials() throws Exception {
+            User failAdd = new User();
+            failAdd.setUserId(0);
+            failAdd.setUsername(TestHelper.goodUsername);
+            failAdd.setEmail(TestHelper.goodEmail);
+            failAdd.setPassword(TestHelper.badFormatPasswordNoSpecial);
+
+            Result<User> expectedResult = new Result<>();
+            expectedResult.addErrorMessage("Password must contain at least one uppercase letter, one lowercase letter, one special character, and one number.", ResultType.INVALID);
+
+            when(service.create(failAdd)).thenReturn(expectedResult);
+
+            mvc.perform(MockMvcRequestBuilders.post(PREFIX)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(jsonMapper.writeValueAsString(failAdd)))
+                    .andExpect(status().isBadRequest());
+        }
+        @Test
+        void shouldReturn400OnInvalidPasswordTooShort() throws Exception {
+            User failAdd = new User();
+            failAdd.setUserId(0);
+            failAdd.setUsername(TestHelper.goodUsername);
+            failAdd.setEmail(TestHelper.goodEmail);
+            failAdd.setPassword(TestHelper.badFormatPasswordShort);
+
+            Result<User> expectedResult = new Result<>();
+            expectedResult.addErrorMessage("Password must be at least 8 characters long.", ResultType.INVALID);
+
+            when(service.create(failAdd)).thenReturn(expectedResult);
+
+            mvc.perform(MockMvcRequestBuilders.post(PREFIX)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(jsonMapper.writeValueAsString(failAdd)))
+                    .andExpect(status().isBadRequest());
         }
     }
 
