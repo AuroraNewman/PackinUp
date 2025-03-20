fda6cd9e81476da618d68f05d1b50c0b75908069
Aurora Newman <auroranewmansoftware@gmail.com>
2025-03-20 13:10:22 -0400

    trip type and template tables added to test schema and prod schema; set known good state updated to have values of both trip type and template

--- server/sql/prod-schema.sql
+++ server/sql/prod-schema.sql
@@ -8,3 +8,19 @@ create table users (
     email varchar(100) unique,
     `password` text
 );
+
+create table trip_types (
+    trip_type_id int primary key auto_increment,
+    trip_type_name varchar(50) unique,
+    trip_type_description text
+    );
+
+create table templates (
+    template_id int primary key auto_increment,
+    template_name varchar(50) unique,
+    template_description text,
+    template_trip_type_id int,
+    foreign key (template_trip_type_id) references trip_types(trip_type_id),
+    template_user_id int,
+    foreign key (template_user_id) references users(user_id)
+);
