c41a9e985c497501febab25adcc203e719f41817
Aurora Newman <auroranewmansoftware@gmail.com>
2025-03-23 16:31:37 -0400

    can post templateitems from the front end using a simplified DTO that finds the item and template using the id

--- server/src/main/java/learn/controllers/TemplateItemController.java
+++ server/src/main/java/learn/controllers/TemplateItemController.java
@@ -3,9 +3,14 @@ package learn.controllers;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
+import learn.data_transfer_objects.IncomingTemplateItem;
+import learn.models.Item;
+import learn.models.Template;
 import learn.models.TemplateItem;
+import learn.service.ItemService;
 import learn.service.Result;
 import learn.service.TemplateItemService;
+import learn.service.TemplateService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,11 +24,16 @@ import java.util.Map;
 public class TemplateItemController {
     private final TemplateItemService service;
     private final SecretSigningKey secretSigningKey;
+    private final TemplateService templateService;
+    private final ItemService itemService;
 
-    public TemplateItemController(TemplateItemService service, SecretSigningKey secretSigningKey) {
+    public TemplateItemController(TemplateItemService service, SecretSigningKey secretSigningKey, TemplateService templateService, ItemService itemService) {
         this.service = service;
         this.secretSigningKey = secretSigningKey;
+        this.templateService = templateService;
+        this.itemService = itemService;
     }
+
     @GetMapping("/{templateId}")
     ResponseEntity<Object> findAllByTemplateId(@PathVariable int templateId, @RequestHeader Map<String, String> headers) {
         Integer userId = getUserIdFromHeaders(headers);
@@ -40,18 +50,26 @@ public class TemplateItemController {
     }
 
     @PostMapping
-    ResponseEntity<Object> create(@RequestBody TemplateItem templateItem, @RequestHeader Map<String, String> headers) {
+    ResponseEntity<Object> create(@RequestBody IncomingTemplateItem incomingTemplateItem, @RequestHeader Map<String, String> headers) {
         Integer userId = getUserIdFromHeaders(headers);
         if (userId == null) {
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
-        Result<TemplateItem> result = service.create(templateItem);
+        Result<TemplateItem> result = service.create(createTemplateItemFromIncoming(incomingTemplateItem));
 
         if (result.isSuccess()) {
             return new ResponseEntity<>(result.getPayload(), HttpStatus.CREATED);
         } else {
             return new ResponseEntity<>(result.getErrorMessages(), HttpStatus.BAD_REQUEST);
         }
+    }
+
+    private TemplateItem createTemplateItemFromIncoming(IncomingTemplateItem incomingTemplateItem) {
+        Item item = itemService.findById(incomingTemplateItem.getTemplateItemItemId());
+        Template template = templateService.findById(incomingTemplateItem.getTemplateItemTemplateId()).getPayload();
+        int quantity = incomingTemplateItem.getTemplateItemQuantity();
+        boolean isChecked = incomingTemplateItem.isTemplateItemIsChecked();
+        return new TemplateItem(quantity, isChecked, template, item);
     }
     private Integer getUserIdFromHeaders(Map<String, String> headers) {
         if (headers.get("authorization") == null) {
